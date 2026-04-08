509c777d23cce4fe62bf3ad6ce74b5bfbd0b88e3
SpaceSpaceWandererr <jsinhthakur@gmail.com>
2026-04-08 23:39:03 +0530

    fix cron ping setup

--- api/ping.js
+++ api/ping.js
@@ -1,8 +1,16 @@
 export default async function handler(req, res) {
   try {
-    await fetch("https://fitness-backend-laoe.onrender.com/api/test");
+    const response = await fetch(
+      "https://fitness-backend-laoe.onrender.com/api/test",
+    );
+
+    if (!response.ok) {
+      throw new Error("Backend not OK");
+    }
+
     res.status(200).send("Ping success");
-  } catch {
+  } catch (err) {
+    console.error(err);
     res.status(500).send("Ping failed");
   }
 }
